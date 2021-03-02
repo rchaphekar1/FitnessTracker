4553a1713f88da96d33b14357e85db8431eefaf0
Rahul Chaphekar <rchaphekar1@gmail.com>
2021-03-01 23:09:17 -0600

    Updated server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,3 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 3000
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
     {
@@ -7,3 +19,9 @@ mongoose.connect(
       useFindAndModify: false
     }
   );
+
+app.use(require('./routes/api.js'));
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`)
+})
