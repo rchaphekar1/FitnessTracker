3b6f71fb8a2274eed42318dde7dab7995b9b9d18
Rahul Chaphekar <rchaphekar1@gmail.com>
2021-03-01 22:52:12 -0600

    Added server.js

---
+++ Develop/server.js
@@ -0,0 +1,9 @@
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
